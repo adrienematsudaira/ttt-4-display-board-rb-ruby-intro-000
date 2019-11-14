b135a8a19a82fc721692fb5a8d76028d40e6ecf6
Adriene Matsudaira <adriene.matsudaira@wework.com>
2019-11-14 03:50:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,5 +7,5 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
   board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-  puts "#{board[4]}"
+  puts "display_board#{board[4]}"
 end
